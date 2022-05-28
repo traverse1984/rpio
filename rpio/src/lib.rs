4a1593666672229d55ef7d80b897b737f02d9586
mwilson <matthew.wilson@datacenta.net>
2022-05-28 20:57:26 +0100

    Add flash to main rpio crate

--- rpio/src/lib.rs
+++ rpio/src/lib.rs
@@ -6,5 +6,8 @@ pub use rpio_gpio::{self as gpio, *};
 #[cfg(feature = "spi")]
 pub use rpio_spi as spi;
 
+#[cfg(feature = "flash")]
+pub use rpio_flash as flash;
+
 #[cfg(feature = "devices")]
 pub use rpio_dev::{self as dev, *};
